767918edceadfaaa902f2695bbb775a97bbee935
Matthew Lund <lundmatthewj@gmail.com>
2024-03-13 17:17:24 -0600

    Now repeatedly accepts user input until quit

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -4,4 +4,10 @@ public class Client {
     public Client(String url) {
 
     }
+    public String eval(String input) {
+        return switch (input) {
+            case "quit" -> "quit";
+            default -> "default";
+        };
+    }
 }
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -10,11 +10,26 @@ public class Repl {
         this.client = new Client(url);
     }
     public void run() {
+        System.out.println(BLACK_KNIGHT + "Welcome to Chess240!");
+        System.out.print("<<help placeholder>>");
         Scanner scanner = new Scanner(System.in);
-        promptUserInput();
-        String line = scanner.next();
+        String result = "";
+        while (!result.equals("quit")) {
+            promptUserInput();
+            String line = scanner.nextLine();
+            try {
+                result = client.eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
     }
+
+
     private void promptUserInput() {
-        System.out.print("\n>>> " + RESET_TEXT_BOLD_FAINT + SET_TEXT_COLOR_GREEN);
+        System.out.print(SET_TEXT_COLOR_WHITE + "\n>>> " + RESET_TEXT_BOLD_FAINT + SET_TEXT_COLOR_GREEN);
     }
 }
