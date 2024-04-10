a78355075a0b21a99135509581f856d8664b3902
Matthew Lund <lundmatthewj@gmail.com>
2024-04-09 20:43:29 -0500

    Added ChessMove argument to MakeMove constructor

--- shared/src/main/java/webSocketMessages/userCommands/MakeMove.java
+++ shared/src/main/java/webSocketMessages/userCommands/MakeMove.java
@@ -1,8 +1,15 @@
 package webSocketMessages.userCommands;
 
+import chess.ChessMove;
+
 public class MakeMove extends UserGameCommand {
-    public MakeMove(String authToken, int gameID) {
+    private final ChessMove move;
+    public MakeMove(String authToken, int gameID, ChessMove move) {
         super(authToken, gameID);
         commandType = CommandType.MAKE_MOVE;
+        this.move = move;
+    }
+    public ChessMove getMove() {
+        return move;
     }
 }
